d4b5fe453f9b55167a2a076fe90c9f07f5e347f4
ImamRenaldy <imam.renaldy26@gmail.com>
2024-08-08 11:44:08 +0700

    change LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./Pages/Auth/LoginPage";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
